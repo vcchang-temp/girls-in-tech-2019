a177b4dfa5e3033172c53b030a40b1318e36d2da
vcchang <vcchang@users.noreply.github.com>
2019-05-04 16:51:50 -0700

    Change to have a fcn for plant and remove trees instead

--- src/Main.js
+++ src/Main.js
@@ -2,26 +2,26 @@ let goodCompanyTrees = {};
 let badCompanyTreeCount;
 
 
-function submit(checkboxVal, noOfTrees)
-{
-	if (checkboxVal)
-	{
-		//Good Company logic
-		executeGoodCompanyLogic(noOfTrees);
-	}
-	else
-	{
-		//Bad Company Logic
-		executeBadCompanyLogic(noOfTrees);
-	}
-}
-
-function executeGoodCompanyLogic(noOfTrees) 
+function plantTrees(noOfTrees)
 {
 
 }
 
-function executeBadCompanyLogic(noOfTrees) 
+function removeTrees(noOfTrees) 
 {
+	// lead user to second page
+	// on second pg, display badCompanyTreeCount, good company name and no of trees > 0, swipe left button, and swipe right button w/ input box
+	// if user inputs number and presses swipe right, then call swipeRight
+	// if user inputs presses swipe left, then call swipeLeft
+			// move to next company
+			// keep going until badCompanyTreeCount === 0
+}
 
+function swipeRight(noOfTrees) {
+	// check if input no <= good company tree count
+	// decrement good co tree count and badCompanyTreeCount by input
+}
+
+function swipeLeft(noOfTrees) {
+	// display next company
 }
