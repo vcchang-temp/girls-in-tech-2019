70e646f99661da912c9d8f7d359117a74d999992
b3z9a <jennabains@gmail.com>
2019-05-04 17:07:15 -0700

    Implementnig goodCompanyTrees

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,14 @@
 let goodCompanyTrees = {};
-let badCompanyTreeCount;
+let goodCompanyTreeCount = 0;
+let badCompanyTreeCount = 0;
 
 
-function submit(checkboxVal, noOfTrees)
+function submit(checkboxVal, companyName, noOfTrees)
 {
 	if (checkboxVal)
 	{
 		//Good Company logic
-		executeGoodCompanyLogic(noOfTrees);
+		executeGoodCompanyLogic(companyName, noOfTrees);
 	}
 	else
 	{
@@ -16,9 +17,18 @@ function submit(checkboxVal, noOfTrees)
 	}
 }
 
-function executeGoodCompanyLogic(noOfTrees) 
+function executeGoodCompanyLogic(companyName, noOfTrees)
 {
+	if (companyName in goodCompanyTrees) {
 
+		goodCompanyTrees[companyName] = noOfTrees;
+        goodCompanyTreeCount += noOfTrees;
+
+	} else {
+        // otherwise insert new key value pair
+        goodCompanyTrees[companyName] = noOfTrees;
+        goodCompanyTreeCount += noOfTrees;
+	}
 }
 
 function executeBadCompanyLogic(noOfTrees) 
